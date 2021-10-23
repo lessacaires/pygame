c2ccc20eebe3c01621fccecd8bf33d1a934563e8
lessacaires <lessacaires@gmail.com>
2021-10-23 01:08:50 -0300

    Completando a sessão level

--- main.py
+++ main.py
@@ -7,11 +7,24 @@ class Game:
 
     def __init__(self):
         self.max_level = 2
-        self.overworld = Overworld(1, self.max_level, screen)
-        self.level = Level(1, screen)
+        self.overworld = Overworld(1, self.max_level, screen, self.create_level)
+        self.status = 'overworld'
+
+    def create_level(self, current_level):
+        self.level = Level(current_level, screen, self.create_overworld)
+        self.status = 'level'
+
+    def create_overworld(self, current_level, new_max_level):
+        if new_max_level > self.max_level:
+            self.max_level = new_max_level
+        self.overworld = Overworld(current_level, self.max_level,screen,self.create_level)
+        self.status = 'overworld'
 
     def run(self):
-        self.level.run()
+        if self.status == 'overworld':
+            self.overworld.run()
+        else:
+            self.level.run()
 
 
 pygame.init()
